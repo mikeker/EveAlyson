9ca9ccaeccf34c074ba9cb37a8a15bcab04404a2
Mike Keran <info@mikekeran.com>
2012-10-01 10:33:42 -0700

    Testing

--- evealyson.js
+++ evealyson.js
@@ -1,10 +1,8 @@
-<script type="text/javascript">
 (function ($) {
   Drupal.behaviors.eveAlyson = {
     attach: function(context) {
-      alert('hi');
       $('.powered-by-drupal-gardens-text').hide();
     }
   }
 }) (jQuery);
-</script>
+
