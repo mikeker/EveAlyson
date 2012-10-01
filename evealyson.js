d7dc33debf7d5315aed86837964f1f13552bc6ea
Mike Keran <info@mikekeran.com>
2012-10-01 10:05:19 -0700

    Initial checkin

---
+++ evealyson.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+(function ($) {
+  Drupal.behaviors.eveAlyson = {
+    attach: function(context) {
+      $('.powered-by-drupal-gardens-text').hide();
+    }
+  }
+}) (jQuery);
+</script>
